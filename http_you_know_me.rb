fa92cb43380f2c8e25ae3cf19c53d8d4cb1bb810
Jordan Lawler <jordan.lawler@colorado.edu>
2015-11-02 16:41:56 -0700

    Made iteration into own class

--- http_you_know_me.rb
+++ http_you_know_me.rb
@@ -2,29 +2,39 @@ require 'socket'
 
 tcp_server = TCPServer.new(9292)
 
-count = 0
 
-loop do
+class YouKnowMeServer
 
-  client = tcp_server.accept
-  line = client
+  count = 0
 
-  puts "Sending Response..."
+  loop do
 
-  response = "<pre>" + "Hello_World (#{count})\nResend request to up counter" + line + "<pre>"
-  output = "<html><head></head><body>#{response}</body></html>"
+    client = tcp_server.accept
+    puts "Ready for Request:"
 
-  headers = ["http/1.1 200 ok",
-            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-            "server: ruby",
-            "content-type: text/html; charset=iso-8859-1",
-            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
 
-  count += 1
+    puts "Sending Response..."
 
-  client.puts headers
-  client.puts output
+    response = "<pre>" + "Hello_World (#{count})\nResend request to up counter\n" + request_lines.to_s + "<pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+
+    headers = ["http/1.1 200 ok",
+              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+
+    count += 1
+
+    client.puts headers
+    client.puts output
+
+  end
+
+  client.close
 
 end
-
-client.close
